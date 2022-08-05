7a5dc3e6d8a34b98c5caf5397d80cab7826da6b0
omarbassatni <omarbassatni@gmail.com>
2022-08-06 02:12:26 +0300

    finished

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(array,event){
+    let eventArray =[]
+    for (let index = 0; index < array.length; index++) {
+        eventArray[index]=`Thank you, ${array[index]}, for the wonderful ${event} gift!`
+    }
+    return eventArray
+}
+
+function countDown(number){
+    let i=0
+    while(i<=number){
+        console.log(number)
+        number--
+    
+    }
+    
+
+}
